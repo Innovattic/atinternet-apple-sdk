c67a6bf136eb487bafea9755aae05dbabbda4878
Nicolas Sagnette <nicolas.sagnette@atinternet.com>
2020-10-08 11:10:52 +0200

    feature: add proxying configuration to send hit

--- ATInternetTracker/Sources/TechnicalContext.swift
+++ ATInternetTracker/Sources/TechnicalContext.swift
@@ -57,9 +57,9 @@ class TechnicalContext: NSObject {
     class var sdkVersion: String {
         get {
             #if os(watchOS) || os(tvOS)
-            return "1.16.0"
+            return "1.17.0"
             #else
-            return "2.19.0"
+            return "2.20.0"
             #endif
         }
     }
